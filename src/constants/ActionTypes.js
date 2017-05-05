db4521685be82244c5495fb5652fdc6313c60e36
Tybi <developer@dwastudio.fr>
2017-05-05 07:37:09 +0200

    add api.js & actions types

--- src/constants/ActionTypes.js
+++ src/constants/ActionTypes.js
@@ -1,2 +1,6 @@
 export const PUSH = 'PUSH'
-export const POP = 'POP'  
+export const POP = 'POP'
+
+export const FETCH_START = 'FETCH_START'
+export const FETCH_DONE = 'FETCH_DONE'
+export const FETCH_FAILED = 'FETCH_FAILED'
---
+++ src/constants/api.js
@@ -0,0 +1,1 @@
+export default apiKey = "YOUR API KEY";  
